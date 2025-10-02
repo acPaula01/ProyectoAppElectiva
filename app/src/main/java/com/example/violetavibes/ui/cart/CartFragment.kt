dbdff1d2d66feef68e7e96071a62a7e20d3981bb
DanielaA23 <damaya35@uan.edu.co>
2025-10-01 23:07:05 -0500

    actualizacion pestaña inicio- se puede navegar

---
+++ app/src/main/java/com/example/violetavibes/ui/cart/CartFragment.kt
@@ -0,0 +1,18 @@
+package com.example.violetavibes.ui.cart
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.violetavibes.R
+
+class CartFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_cart, container, false)
+    }
+}
