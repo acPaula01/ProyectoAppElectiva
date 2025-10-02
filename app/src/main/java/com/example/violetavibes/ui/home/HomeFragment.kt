c047ffc809b3a947fdb0fa7e0b33b5091182b216
DanielaA23 <damaya35@uan.edu.co>
2025-10-02 00:05:03 -0500

    navegación, funcion detalle producto

--- app/src/main/java/com/example/violetavibes/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/violetavibes/ui/home/HomeFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.violetavibes.R
 
 class HomeFragment : Fragment() {
@@ -15,5 +17,17 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Buscar el botón en el layout
+        val btnGoToDetail: Button = view.findViewById(R.id.btnGoToDetail)
+
+        // Acción al hacer clic → navegar al fragmento de detalle
+        btnGoToDetail.setOnClickListener {
+            findNavController().navigate(R.id.productDetailFragment)
+        }
     }
 }
